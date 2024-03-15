8a1b450c3b72864415b0153fae682b02542ad2c1
“kevin.chen” <kevin.chen@medicrowd.com.tw>
2024-03-15 10:42:20 +0800

    composition api

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -14,14 +14,16 @@
 
 <script>
 export default {
-  data() {
+  setup() {
+    const transactions = [
+      { id: 1, text: "Flower", amount: -19.99 },
+      { id: 2, text: "Salary", amount: 299.97 },
+      { id: 3, text: "Book", amount: -10 },
+      { id: 4, text: "Camera", amount: 150 },
+    ];
+
     return {
-      transactions: [
-        { id: 1, text: "Flower", amount: -19.99 },
-        { id: 2, text: "Salary", amount: 299.97 },
-        { id: 3, text: "Book", amount: -10 },
-        { id: 4, text: "Camera", amount: 150 },
-      ],
+      transactions,
     };
   },
 };
